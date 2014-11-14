5b68445ba48b35c0bd79f25b4ba55a3fcdc8952f
ksripathi <kammari.sripathi@gmail.com>
2014-11-14 11:21:56 +0530

    Update install_openvz.sh

--- scripts/install_openvz.sh
+++ scripts/install_openvz.sh
@@ -43,17 +43,17 @@ else
 fi
 
 # Not needed!? Not sure.
-#echo "Allowing multiple subnets to reside on the same network interface.."
-#sed -i 's/#NEIGHBOUR_DEVS=all/NEIGHBOUR_DEVS=all/g' /etc/vz/vz.conf
-#sed -i 's/NEIGHBOUR_DEVS=detect/NEIGHBOUR_DEVS=all/g' /etc/vz/vz.conf
-#
-#echo "Setting container layout to default to ploop (VM in a file).."
-#sed -i 's/#VE_LAYOUT=ploop/VE_LAYOUT=ploop/g' /etc/vz/vz.conf
-#
-#echo "Setting Ubuntu 12.04 64bit to be the default template.."
-#sed -i 's/centos-6-x86/ubuntu-12.04-x86_64/g' /etc/vz/vz.conf
-#
-#sysctl -p
+echo "Allowing multiple subnets to reside on the same network interface.."
+sed -i 's/#NEIGHBOUR_DEVS=all/NEIGHBOUR_DEVS=all/g' /etc/vz/vz.conf
+sed -i 's/NEIGHBOUR_DEVS=detect/NEIGHBOUR_DEVS=all/g' /etc/vz/vz.conf
+
+echo "Setting container layout to default to ploop (VM in a file).."
+sed -i 's/#VE_LAYOUT/VE_LAYOUT=ploop/g' /etc/vz/vz.conf
+
+echo "Setting Ubuntu 12.04 64bit to be the default template.."
+sed -i 's/centos-6-x86/ubuntu-12.04-x86_64/g' /etc/vz/vz.conf
+
+sysctl -p
 #
 #echo "Disabling iptables.."
 #/etc/init.d/iptables stop && chkconfig iptables off
